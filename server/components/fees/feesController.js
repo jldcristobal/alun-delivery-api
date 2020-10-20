b8a43693dc2edc865e29bf0019f9ce1b6cf1d377
Jodie Cristobal <jldp.cristobal@questronix.com.ph>
2020-10-20 08:24:46 +0800

    Update Delivery Fee Calculator pricing rules

--- server/components/fees/feesController.js
+++ server/components/fees/feesController.js
@@ -21,15 +21,104 @@ fees.getByDistance = (req, res) => {
   let jsonRes;
   
   const body = req.body
-  var locationFrom = body.deliverFrom.city + ', ' + body.deliverFrom.barangay
-  var locationTo = body.deliverTo.city + ', ' + body.deliverTo.barangay
+  const perishable = body.perishable
+  const vehicleType = body.vehicleType
+  var pickUp = body.deliverFrom.city + ', ' + body.deliverFrom.barangay
+  var dropOff = body.deliverTo.city + ', ' + body.deliverTo.barangay
 
-  var locations = [locationFrom, locationTo].sort()
-  locationFrom = locations[0].split(', ')
-  locationTo = locations[1].split(', ')
+  var baseFare
+  var afterBaseFare
+
+  if(!perishable) {
+    switch(vehicleType) {
+      case('motorcycle'):
+        baseFare = 60
+        afterBaseFare = 10
+        break
+      case('tricycle'):
+        baseFare = 100
+        afterBaseFare = 10
+        break
+      case('multicab'):
+        baseFare = 260
+        afterBaseFare = 15
+        break
+      case('private'):
+        baseFare = 260
+        afterBaseFare = 20
+        break
+    }
+  } else {
+    switch(vehicleType) {
+      case('motorcycle'):
+        switch(body.deliverFrom.city) {
+          case('Iriga'):
+            baseFare = 40
+            break
+          case('Nabua'):
+            baseFare = 50
+            break
+          case('Bato'):
+            baseFare = 70
+            break
+          case('Buhi'):
+            baseFare = 80
+            break
+          case('Baao'):
+            baseFare = 60
+            break
+        }
+        afterBaseFare = 5
+        break
+      case('tricycle'):
+        switch(body.deliverFrom.city) {
+          case('Iriga'):
+            baseFare = 60
+            break
+          case('Nabua'):
+            baseFare = 70
+            break
+          case('Bato'):
+            baseFare = 90
+            break
+          case('Buhi'):
+            baseFare = 100
+            break
+          case('Baao'):
+            baseFare = 80
+            break
+        }
+        afterBaseFare = 10
+        break
+      case('multicab'):
+        switch(body.deliverFrom.city) {
+          case('Iriga'):
+            baseFare = 220
+            break
+          case('Nabua'):
+            baseFare = 230
+            break
+          case('Bato'):
+            baseFare = 250
+            break
+          case('Buhi'):
+            baseFare = 260
+            break
+          case('Baao'):
+            baseFare = 240
+            break
+        }
+        afterBaseFare = 15
+        break
+    }
+  }
+
+  var locations = [pickUp, dropOff].sort()
+  pickUp = locations[0].split(', ')
+  dropOff = locations[1].split(', ')
   
-  let one = locationFrom[1] + ', ' + locationFrom[0]
-  let two = locationTo[1] + ', ' + locationTo[0]
+  let one = pickUp[1] + ', ' + pickUp[0]
+  let two = dropOff[1] + ', ' + dropOff[0]
   
   const query = `SELECT direct_distance FROM rinconadaDD WHERE one = "${one}" AND two = "${two}"`
   
@@ -38,45 +127,12 @@ fees.getByDistance = (req, res) => {
 
   getDirectDistance.then((distance) => {
     if(distance.length > 0 || one === two) {
-      if(one === two) directDistance = 0
+      if(one === two) directDistance = 1
       else directDistance = Math.round(distance[0].direct_distance)
       
-      let deliveryFee;
-      
-      switch(true) {
-        case(directDistance >= 0 && directDistance <= 1):
-          deliveryFee = 30
-          break
-        case(directDistance >= 2 && directDistance <= 3):
-          deliveryFee = 45
-          break
-        case(directDistance >= 4 && directDistance <= 5):
-          deliveryFee = 59
-          break
-        case(directDistance == 6):
-          deliveryFee = 65
-          break
-        case(directDistance == 7):
-          deliveryFee = 69
-          break
-        case(directDistance == 8):
-          deliveryFee = 79
-          break
-        case(directDistance == 9):
-          deliveryFee = 89
-          break
-        case(directDistance == 10):
-          deliveryFee = 110
-          break
-        case(directDistance >= 11 && directDistance <= 15):
-          deliveryFee = 150
-          break
-        case(directDistance >= 16 && directDistance <= 20):
-          deliveryFee = 200
-          break
-        default:
-          break
-      }
+      if(directDistance === 0) directDistance++
+
+      const deliveryFee = baseFare + (directDistance - 1) * afterBaseFare;
 
       jsonRes = {
         statusCode: 200,
