ea9bc8b70d4578d08c281f4485c695381d155e0c
Ron Mesa <ryelizarov@yandex.com>
2020-11-12 15:33:26 +0800

    Order API updates: Automatic follow up

--- server/components/order/orderController.js
+++ server/components/order/orderController.js
@@ -16,12 +16,11 @@ console.log('controllers - userEnrollment')
  */
 const order = {}
 
-const isOrderConfirmed = async (orderUuid) => {
+const getOrderDetails = async (orderUuid) => {
    const query = `SELECT * FROM orders WHERE order_uuid = '${orderUuid}'`
 
    try {
       const status = await mysqlDbHelper.execute(query)
-      //console.log(status[0])
       return status[0]
    } catch (err) {
       console.log({err})
@@ -61,10 +60,14 @@ order.addOrder = async (req, res) => {
    })
 }
 
+/*
+ * Automatic follow up
+ * Send message reminder if order has not been responded within the follow up window
+ */
 order.followUp = (orderUuid) => {
    setTimeout(async () => {
       try {
-         const { order_uuid, order_confirmed, order_number, merchant_contacts } = await isOrderConfirmed(orderUuid)
+         const { order_uuid, order_confirmed, order_number, merchant_contacts } = await getOrderDetails(orderUuid)
          const contactIds = merchant_contacts.split(",")
          if (!order_confirmed) {
             console.log("Order not confirmed")
@@ -78,7 +81,7 @@ order.followUp = (orderUuid) => {
                            messages: [
                               {
                                  type: "text",
-                                 text: `{{first_name}}, please use the buttons below to confirm or send feedback regarding 📋 Order No. ${order_number} \n\nOtherwise, a member of our team will call you after two minutes to manually follow up on the order. Thanks! 😊`,
+                                 text: `⚡ {{first_name}}, Order No. ${order_number} is still pending. Please use the buttons below to confirm or send feedback regarding this order. \n\nOtherwise, a member of our team will call you after two minutes to manually follow up on the order. Thanks! 😊`,
                                  buttons: [
                                     {
                                        type: "dynamic_block_callback",
@@ -92,7 +95,9 @@ order.followUp = (orderUuid) => {
                                           orderUuid: order_uuid,
                                           orderNumber: order_number,
                                           updateType: "confirm",
-                                          confirmerId: "{{id}}"
+                                          confirmerId: "{{user_id}}",
+                                          currentUser: "{{first_name}}",
+                                          confirmedBy: "{{first_name}}"
                                        }
                                     },
                                     {
@@ -107,7 +112,9 @@ order.followUp = (orderUuid) => {
                                           orderUuid: order_uuid,
                                           orderNumber: order_number,
                                           updateType: "feedback",
-                                          confirmerId: "{{id}}"
+                                          confirmerId: "{{user_id}}",
+                                          currentUser: "{{first_name}}",
+                                          confirmedBy: "{{first_name}}"
                                        }
                                     }
                                  ]
@@ -126,6 +133,7 @@ order.followUp = (orderUuid) => {
 
                   if (response.data.status == "success") {
                      console.log("Message sent!")
+                     order.finalPrompt(orderUuid)
                   } else {
                      console.log("Message not sent!")
                   }
@@ -134,7 +142,7 @@ order.followUp = (orderUuid) => {
                }
             })
          } else {
-            console.log("Order already confirmed! No need to follow up!")
+            console.log(`Order No. ${order_number} already confirmed! No need to follow up!`)
          }
       } catch (err) {
          console.log("Error: ", err)
@@ -142,102 +150,43 @@ order.followUp = (orderUuid) => {
    }, config.followUpWindow)
 }
 
-/**
- * Retrieve order details
+/*
+ * Automatic follow up
+ * Send final prompt if no response is received after previous message reminder
  */
-order.getOrder = async (req, res) => {
-
-   let jsonRes
-   const { orderUuid } = req.body
-   const query = `SELECT * FROM orders WHERE order_uuid = "${orderUuid}"`
-
-   let getOrder = mysqlDbHelper.execute(query)
-
-   const formatCart = (orders) => {
-      var text = ""
-      var cart = JSON.parse(orders)[0].cart
-      //console.log(cart[0].merchant)
-      for (var item of cart) {
-         Object.keys(item).forEach((key) => {
-            if (key == "item") text += "📌 " + item[key] + "\n"
-            else text +=  item[key] + "\n"
-         })
-         text += "\n"
-      }
-      return text
-   }
-
-   getOrder.then((result) => { 
-
-      /*jsonRes = {
-         statusCode: 200,
-         success: true,
-         message: "Order retrieved successfully",
-         result: result
-      };  */
-      const { order_uuid, order_number, order_cart, merchant_name } = result[0]
-
-      jsonRes = {
-         statusCode: 200,
-         version: "v2",
-         content: {
-            messages: [
-               {
-                  type: "text",
-                  text: `👋 Hi {{first_name}}! We received the following order(s) for ${merchant_name}: \n\n ${formatCart(order_cart)}Ref: 📋 Order No. ${order_number}`,
-                  buttons: [
-                     {
-                        type: "dynamic_block_callback",
-                        caption: `Confirm ORD#${order_number} ✅`,
-                        url: "https://dev-api.alun.app/api/order/update",
-                        method: "post",
-                        headers: {
-                           Authorization: "Bearer"
-                        },
-                        payload: {
-                           orderUuid: order_uuid,
-                           orderNumber: order_number,
-                           updateType: "Bearer"
-                        }
-                     },
-                     {
-                        type: "dynamic_block_callback",
-                        caption: "Send feedback 💬",
-                        url: "https://dev-api.alun.app/api/order/update",
-                        method: "post",
-                        headers: {
-                           Authorization: ""
-                        },
-                        payload: {
-                           orderUuid: order_uuid,
-                           orderNumber: order_number,
-                           updateType: "feedback"
-                        }
+order.finalPrompt = (orderUuid) => {
+   setTimeout(async () => {
+      try {
+         const { order_confirmed, order_number, merchant_contacts } = await getOrderDetails(orderUuid)
+         const contactIds = merchant_contacts.split(",")
+         if (!order_confirmed) {
+            console.log("Order still not confirmed!")
+            contactIds.forEach(async (id) => {
+               try {
+                  let response = await axios.post("https://api.manychat.com/fb/sending/sendFlow", {
+                     subscriber_id: id,
+                     flow_ns: "content20201031171458_452545" // Order Prompt - Unconfirmed Order
+                  }, {
+                     headers: {
+                        Authorization: `Bearer ${config.MANYCHAT_API_KEY}`
                      }
-                  ]
+                  })
+                  if (response.data.status == "success") {
+                     console.log("User redirected to Unconfirmed Order flow")
+                  } else {
+                     console.log("User not redirected to Unconfirmed Order flow")
+                  }
+               } catch (e) {
+                  console.log(e)
                }
-            ],
-            actions: [
-               {
-                  action: "set_field_value",
-                  field_name: "MerchantName",
-                  value: merchant_name
-               }
-            ],
-            quick_replies: []
+            })
+         } else {
+            console.log(`Order No. ${order_number} already confirmed! No need to follow up!`)
          }
+      } catch (e) {
+         console.log(e)
       }
-   }).catch((error) => {
-      console.log(error)
-
-      jsonRes = {
-         statusCode: 500,
-         success: false,
-         message: error,
-      }
-   }).finally(() => {
-      util.sendResponse(res, jsonRes)
-   })
+   }, config.followUpWindow)
 }
 
 /**
@@ -246,18 +195,18 @@ order.getOrder = async (req, res) => {
 order.updateOrder = async (req, res) => {
 
    let jsonRes
-   const { orderUuid, orderNumber, updateType, confirmerId } = req.body
-   const query = `UPDATE orders SET order_confirmed = ${1} WHERE order_uuid = "${orderUuid}"`
-
-   let getOrder = mysqlDbHelper.execute(query)
-
-   getOrder.then(async () => { 
-      /*jsonRes = {
-         statusCode: 200,
-         success: true,
-         message: "Order updated successfully"
-      }*/
-      if (updateType === "confirm") {
+   const { orderUuid, orderNumber, updateType, confirmerId, currentUser, confirmedBy } = req.body
+   
+   try {
+      const { order_confirmed, confirmed_by } = await getOrderDetails(orderUuid)
+      if (order_confirmed) {
+         console.log("Order already confirmed by", confirmed_by)
+         let message = ""
+         if (currentUser == confirmed_by) {
+            message = `👍🏼 You have already attended to Order No. ${orderNumber}. Just stand by for future orders! 👌🏻`
+         } else {
+            message = `👍🏼 Order No. ${orderNumber} has already been attended by ${confirmed_by}. Just stand by for future orders! 👌🏻`
+         }
          jsonRes = {
             statusCode: 200,
             version: "v2",
@@ -265,79 +214,104 @@ order.updateOrder = async (req, res) => {
                messages: [
                   {
                      type: "text",
-                     text: `Awesome, {{first_name}}! 🤗 You have confirmed 📋 Order No. ${orderNumber}`,
+                     text: message,
                      buttons: []
                   }
                ],
                actions: [],
                quick_replies: []
             }
-         }
-         console.log("Send Accept Order flow")
-
-         try {
-            let response = await axios.post("https://api.manychat.com/fb/sending/sendFlow", {
-               subscriber_id: confirmerId,
-               flow_ns: "content20201019151906_485264" // Accept Order
-            }, {
-               headers: {
-                  Authorization: `Bearer ${config.MANYCHAT_API_KEY}`
-               }
-            })
-            if (response.data.status == "success") {
-               console.log("User redirected to Accept Order flow")
-            } else {
-               console.log("User not redirected to Accept Order flow")
-            }
-         } catch (e) {
-            console.log(e)
          }
       } else {
-         jsonRes = {
-            statusCode: 200,
-            version: "v2",
-            content: {
-               messages: [
-                  {
-                     type: "text",
-                     text: `No worries {{first_name}}! Please tell us which items are not available on 📋 Order No. ${orderNumber}`,
-                     buttons: []
-                  }
-               ],
-               actions: [],
-               quick_replies: []
-            }
-         }
-         console.log("Send Order Feedback flow")
-         try {
-            let response = await axios.post("https://api.manychat.com/fb/sending/sendFlow", {
-               subscriber_id: confirmerId,
-               flow_ns: "content20201019152011_509316" // Order Feedback
-            }, {
-               headers: {
-                  Authorization: `Bearer ${config.MANYCHAT_API_KEY}`
-               }
-            })
-            if (response.data.status == "success") {
-               console.log("User redirected to Order Feedback flow")
-            } else {
-               console.log("User not redirected to order Feedback flow")
-            }
-         } catch (e) {
-            console.log(e)
-         }
-      }
-   }).catch((error) => {
-      console.log(error)
+         const query = `UPDATE orders SET order_confirmed = ${1}, confirmed_by = "${confirmedBy}" WHERE order_uuid = "${orderUuid}"`
+         let getOrder = mysqlDbHelper.execute(query)
 
-      jsonRes = {
-         statusCode: 500,
-         success: false,
-         message: error,
+         getOrder.then(async () => { 
+            if (updateType === "confirm") {
+               jsonRes = {
+                  statusCode: 200,
+                  version: "v2",
+                  content: {
+                     messages: [
+                        {
+                           type: "text",
+                           text: `Awesome, {{first_name}}! 🤗 You have confirmed 📋 Order No. ${orderNumber}`,
+                           buttons: []
+                        }
+                     ],
+                     actions: [],
+                     quick_replies: []
+                  }
+               }
+               console.log("Send Accept Order flow")
+
+               try {
+                  let response = await axios.post("https://api.manychat.com/fb/sending/sendFlow", {
+                     subscriber_id: confirmerId,
+                     flow_ns: "content20201019151906_485264" // Accept Order
+                  }, {
+                     headers: {
+                        Authorization: `Bearer ${config.MANYCHAT_API_KEY}`
+                     }
+                  })
+                  if (response.data.status == "success") {
+                     console.log("User redirected to Accept Order flow")
+                  } else {
+                     console.log("User not redirected to Accept Order flow")
+                  }
+               } catch (e) {
+                  console.log(e)
+               }
+            } else {
+               jsonRes = {
+                  statusCode: 200,
+                  version: "v2",
+                  content: {
+                     messages: [
+                        {
+                           type: "text",
+                           text: `No worries {{first_name}}! Please tell us which items or ingredients are not available for 📋 Order No. ${orderNumber}`,
+                           buttons: []
+                        }
+                     ],
+                     actions: [],
+                     quick_replies: []
+                  }
+               }
+               console.log("Send Order Feedback flow")
+               try {
+                  let response = await axios.post("https://api.manychat.com/fb/sending/sendFlow", {
+                     subscriber_id: confirmerId,
+                     flow_ns: "content20201019152011_509316" // Order Feedback
+                  }, {
+                     headers: {
+                        Authorization: `Bearer ${config.MANYCHAT_API_KEY}`
+                     }
+                  })
+                  if (response.data.status == "success") {
+                     console.log("User redirected to Order Feedback flow")
+                  } else {
+                     console.log("User not redirected to order Feedback flow")
+                  }
+               } catch (e) {
+                  console.log(e)
+               }
+            }
+         }).catch((error) => {
+            console.log(error)
+
+            jsonRes = {
+               statusCode: 500,
+               success: false,
+               message: error,
+            }
+         }).finally(() => {
+            util.sendResponse(res, jsonRes)
+         })
       }
-   }).finally(() => {
-      util.sendResponse(res, jsonRes)
-   })
+   } catch (e) {
+      console.log(e)
+   }
 }
 
 module.exports = order
